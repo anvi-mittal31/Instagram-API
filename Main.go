2bd5a25c734fa8218c56dc9c679ecd9ae2dbc3bc
anvi-mittal31 <82819020+anvi-mittal31@users.noreply.github.com>
2021-10-09 21:56:38 +0530

    All changes updated.

---
+++ Main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var client *mongo.Client
+
+type User struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"username,omitempty" bson:"username,omitempty"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" bson:"password,omitempty"`
+}
+
+type Post struct {
+	ID               primitive.ObjectID  `json:"_id,omitempty" bson:"_id,omitempty"`
+	Posted_Timestamp primitive.Timestamp `json:"postedTimestamp,omitempty" bson:"postedTimestamp,omitempty"`
+	Image            string              `json:"image,omitempty" bson:"image,omitempty"`
+	Caption          string              `json:"caption,omitempty" bson:"caption,omitempty"`
+}
+
+//Main Function
+
+func main() {
+	fmt.Println("Starting the application...")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, clientOptions)
+	router := mux.NewRouter()
+	router.HandleFunc("/users", CreateUser).Methods("POST")
+	router.HandleFunc("/people", GetUser).Methods("GET")
+	router.HandleFunc("/users/{id}", GetUserID).Methods("GET")
+
+	router.HandleFunc("/posts", CreatePost).Methods("POST")
+	router.HandleFunc("/LatestPosts", GetPost).Methods("GET")
+	router.HandleFunc("/posts/{id}", GetPostID).Methods("GET")
+
+	http.ListenAndServe(":12345", router)
+}
+
+//Creating an user
+
+func CreateUser(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
+	collection := client.Database("InstagramUser").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
+}
+
+//Getting the user by ID
+
+func GetUserID(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var user User
+	collection := client.Database("InstagramUser").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, User{ID: id}).Decode(&user)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(user)
+}
+
+//Creating an array of all the user's structures
+
+func GetUser(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var people []User
+	collection := client.Database("InstagramUser").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var user User
+		cursor.Decode(&user)
+		people = append(people, user)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(people)
+}
+
+//Creating a Post
+
+func CreatePost(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var post Post
+	_ = json.NewDecoder(request.Body).Decode(&post)
+	collection := client.Database("InstagramPost").Collection("LatestPosts")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, post)
+	json.NewEncoder(response).Encode(result)
+}
+
+//Getting the array of all the posts running on the server
+
+func GetPost(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var LatestPosts []Post
+	collection := client.Database("InstagramPost").Collection("LatestPosts")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{"postedTimestamp": time.Now()})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var post Post
+		cursor.Decode(&post)
+		LatestPosts = append(LatestPosts, post)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(LatestPosts)
+}
+
+//Getting the post via ID provided in URL
+
+func GetPostID(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var post Post
+	collection := client.Database("InstagramPost").Collection("LatestPosts")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, Post{ID: id}).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(post)
+}
+
+//Password Authemtication
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Sprintf("login or passowrd is correct")
+	return string(bytes)
+}
+
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("login or passowrd is incorrect")
+		check = false
+	}
+	return check, msg
+}
